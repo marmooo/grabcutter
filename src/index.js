b88d3d9df6c779586102498d3a085649db464e12
marmooo <marmooo@users.noreply.github.com>
2025-03-17 22:21:28 +0900

    improve resizing event

--- src/index.js
+++ src/index.js
@@ -366,6 +366,7 @@ class FilterPanel extends LoadPanel {
     this.paintCanvas.height = actualRect.height;
     this.paintCanvas.style.top = `${actualRect.top}px`;
     this.paintCanvas.style.left = `${actualRect.left}px`;
+    this.paintPad.fromData(this.paintPad.toData());
   }
 
   show() {
